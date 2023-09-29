d6eb39c986779a89aa241118bf1e0aea74870893
vitorkos <vitorkosejt@gmail.com>
2023-09-29 09:51:05 -0300

    test : add post request : return the post body

--- form_backend/routes/api.php
+++ form_backend/routes/api.php
@@ -3,18 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/message_test', function(){
+Route::get('/message-test', function(){
     return response()->json([
         'message' => 'Hello world',
         'status_code' => 200
     ]);
 });
 
-Route::post('/user_signup_test', function(){
-    return response()->json([
-        'name' => 'joãozinho157',
-        'email' => 'vidaloka123@yahoo.com',
-        'senha' => 'senha',
-        'status_code' => 200
-    ]);
+Route::post('/user-sign', function (Request $request) {
+    $request->input('user');
+    return $request;
 });
