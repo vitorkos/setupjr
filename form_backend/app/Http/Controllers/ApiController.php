488aa4b5cf21a96a835c86a8d76ac159c3113463
vitorkos <vitorkosejt@gmail.com>
2023-09-30 14:04:13 -0300

    chore(api:controller) : add api controller : creates data to be storage(request)

--- form_backend/app/Http/Controllers/ApiController.php
+++ form_backend/app/Http/Controllers/ApiController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class ApiController extends Controller
 {
-    //
+    public function create(Request $request){
+        $users = new User();
+
+        $users->name = $request->input('name');
+        $users->email = $request->input('email');
+        $users->password = $request->input('password');
+        $users->cpf = $request->input('cpf');
+
+        $users->save();
+        return response()->json($users);
+    }
 }
