00294a653616d9a29d9e630bd2239e3cadfed513
vitorkos <vitorkosejt@gmail.com>
2023-09-30 14:04:49 -0300

    chore(models) : create User model(class)

--- form_backend/app/Models/User.php
+++ form_backend/app/Models/User.php
@@ -7,5 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    use HasFactory;
+    protected $table = 'users';
+    protected $fillable = ['name', 'email', 'password', 'cpf'];
+    
 }
