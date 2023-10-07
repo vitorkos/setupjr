62bb4231c85ffef44cc246a9c996245bf214e164
vitorkos <vitorkosejt@gmail.com>
2023-10-07 14:35:37 -0300

    chore : change html return for sucess

--- form_backend/app/Http/Controllers/ApiController.php
+++ form_backend/app/Http/Controllers/ApiController.php
@@ -9,13 +9,17 @@ class ApiController extends Controller
 {
     public function create(Request $request){
         $users = new User();
+        $html_return = new User();
 
         $users->name = $request->input('name');
+        $html_return->name = $users->name;
         $users->email = $request->input('email');
+        $html_return->email = $users->email;
         $users->password = $request->input('password');
         $users->cpf = $request->input('cpf');
+        $html_return->cpf = $users->cpf;
 
         $users->save();
-        return response()->json($users);
+        return response()->json($html_return);
     }
 }
