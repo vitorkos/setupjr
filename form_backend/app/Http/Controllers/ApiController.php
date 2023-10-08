5eb2e0d410b1a03600b32a3c1f2e724949061a5a
vitorkos <vitorkosejt@gmail.com>
2023-10-08 15:29:44 -0300

    chore : add validation rules

--- form_backend/app/Http/Controllers/ApiController.php
+++ form_backend/app/Http/Controllers/ApiController.php
@@ -4,14 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-
-function formatter(){
-
-}
+use Illuminate\Validation\Rule;
 
 class ApiController extends Controller
 {
     public function create(Request $request){
+        $rules =[
+            'name' => 'required|string|regex:/^[A-Za-z\s]+$/|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8',
+            'cpf' => 'required|cpf|unique:users|min:11|max:14'
+        ];
+
+
         $users = new User();
 
         $users->name = $request->input('name');
