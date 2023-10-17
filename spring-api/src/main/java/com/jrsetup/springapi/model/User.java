717e28591bdee0d841f3c8941567bd78a398155c
vitorkos <vitorkosejt@gmail.com>
2023-10-17 16:02:16 -0300

    refactor(user-model) : add boilerplate*(study)

--- spring-api/src/main/java/com/jrsetup/springapi/model/User.java
+++ spring-api/src/main/java/com/jrsetup/springapi/model/User.java
@@ -1,16 +1,78 @@
 package com.jrsetup.springapi.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
-
 public class User {
+    private Long id;
     private String name;
     private String email;
     private String password;
     private String cpf;
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String password, String cpf) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.cpf = cpf;
+    }
+
+    public User(String name, String email, String password, String cpf) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.cpf = cpf;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public String toString(){
+        return "User{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", email='" + email + '\'' +
+                    ", password='" + password + '\'' +
+                    ", cpf='" + cpf + '\'' +
+                '}';
+    }
 }
