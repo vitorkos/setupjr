695ac5e270347388ef2d64d66e17181c2bae76db
vitorkos <vitorkosejt@gmail.com>
2023-10-17 14:22:07 -0300

    chore(model) : add user model structure

--- spring-api/src/main/java/com/jrsetup/springapi/model/User.java
+++ spring-api/src/main/java/com/jrsetup/springapi/model/User.java
@@ -1,5 +1,30 @@
 package com.jrsetup.springapi.model;
 
 public class User {
+    String name;
+    String email;
+    String password;
+    String cpf;
 
+    public User(String name, String email, String password, String cpf) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.cpf = cpf;
+    }
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
 }
