74722ed0e5aee03912c98f6fc750e6ae59626dd5
vitorkos <vitorkosejt@gmail.com>
2023-10-18 11:59:00 -0300

    refactor(UserController) : module controller : autowired to UserService

--- spring-api/src/main/java/com/jrsetup/springapi/controller/UserController.java
+++ spring-api/src/main/java/com/jrsetup/springapi/controller/UserController.java
@@ -2,22 +2,26 @@ package com.jrsetup.springapi.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.jrsetup.springapi.model.User;
+import com.jrsetup.springapi.service.UserService;
 
 @RestController
-@RequestMapping
+@RequestMapping(path = "api/v1/user")
 public class UserController {
+
+	private final UserService userService;
+
+	@Autowired
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+
 	@GetMapping
-	public List<User> test(){
-		return List.of(
-			new User("caralho",
-			"test@gmail.com",
-			"123",
-			"234")
-		);
+	public List<User> getUsers(){
+		return userService.getUsers();
 	};
 }
