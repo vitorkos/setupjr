ff5f7986777b7728b23ba8a56490fcaf2849550f
vitorkos <vitorkosejt@gmail.com>
2023-10-18 12:01:30 -0300

    chore(UserService) : create service : UserService

--- spring-api/src/main/java/com/jrsetup/springapi/service/UserService.java
+++ spring-api/src/main/java/com/jrsetup/springapi/service/UserService.java
@@ -1,5 +1,18 @@
 package com.jrsetup.springapi.service;
+import java.util.List;
+import org.springframework.stereotype.Service;
 
+import com.jrsetup.springapi.model.User;
+
+@Service
 public class UserService {
-    
+
+    public List<User> getUsers(){
+		return List.of(
+			new User("caralho",
+			"test@gmail.com",
+			"123",
+			"234")
+		);
+	};
 }
