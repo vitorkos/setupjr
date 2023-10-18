1696089f3503f9e96df7996bf93c13052d171c46
vitorkos <vitorkosejt@gmail.com>
2023-10-18 09:41:24 -0300

    chore(UserController) : create UserController : GetRequest

--- spring-api/src/main/java/com/jrsetup/springapi/controller/UserController.java
+++ spring-api/src/main/java/com/jrsetup/springapi/controller/UserController.java
@@ -1,5 +1,23 @@
 package com.jrsetup.springapi.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jrsetup.springapi.model.User;
+
+@RestController
+@RequestMapping
 public class UserController {
-    
+	@GetMapping
+	public List<User> test(){
+		return List.of(
+			new User("caralho",
+			"test@gmail.com",
+			"123",
+			"234")
+		);
+	};
 }
